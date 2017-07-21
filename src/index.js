bc7af2a8d3dd1d1e043f05a1d0a171b02b225746
vsanchezrod <v.sanchez.rod@gmail.com>
2017-07-21 16:37:53 +0200

    Estructura de los componentes. Creacion del componente de la barra de busqueda (SearchBa)r

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Creamos una constante, que va a ser la KEY DEL API de Youtube. No va a cambiar
+const API_KEY = 'AIzaSyABvZR95mHIj30vRfV0OzKhVJPSg03oYUo';
+
 
 // Creamos un nuevo componente que producirá cídigo HTML
 // Create a new Compoment. This Compoment should produce some HTML
@@ -15,4 +18,4 @@ const App = function () {
 // Tenemos que hacer que meta ese componente nuevo creado en la página (en el DOM)
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
