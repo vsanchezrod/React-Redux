2e26f56bd2c491dcd6e7e41a7200de2ee29aae1d
vsanchezrod <v.sanchez.rod@gmail.com>
2017-07-23 18:58:25 +0200

    Cambiando functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
+// Aquí definiamos un componente funcional, pero ahora lo vamos a convetir en un componente de clase
+/*const SearchBar = () => {
     return <input />;
+}*/
+
+// Creo la clase
+class SearchBar extends Component {
+    // Creo el metodo para el render (es como una función y devuelve código HTML)
+    render (){
+        return <input />;
+    }
+
 }
+
+
+
+
+
 
 export default SearchBar;
