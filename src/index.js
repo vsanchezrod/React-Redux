fd0cecd90dce893963c8ca00a45c9c2e1f31e1ac
vsanchezrod <v.sanchez.rod@gmail.com>
2017-08-11 17:35:34 +0200

    Refactorizando App de Functional Component a Class Component

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import YTSearch from 'youtube-api-search';
 // Importamos el componente de la barra de busqueda SearcBar
 import SearchBar from './components/search_bar';
 
-YTSearch({key: API_KEY, termn: 'surfboards'}, function(data) {console.log(data)});
-
 // Creamos una constante, que va a ser la KEY DEL API de Youtube. No va a cambiar
 const API_KEY = 'AIzaSyABvZR95mHIj30vRfV0OzKhVJPSg03oYUo';
 
 
-// Creamos un nuevo componente que producirá cídigo HTML
-// Create a new Compoment. This Compoment should produce some HTML
-const App = function () {
-  return (
-    <div>
-      <SearcBar />
-    </div>
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos:[] };
+    YTSearch({key: API_KEY, termn: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      //this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearcBar />
+      </div>
     );
+  }
 }
-
-
-
-
 
 // Tenemos que hacer que meta ese componente nuevo creado en la página (en el DOM)
 // Take this component's generated HTML and put it on the page (in the DOM)
